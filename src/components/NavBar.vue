2cba1d95dca54b116a2b4172d5b870b7ef94adab
Vladimir Petrovic <petrovic.vladimir92@gmail.com>
2018-05-08 18:26:38 +0200

    Created nav bar and added cars link

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <router-link class="navbar-brand" to="/">Cars App</router-link>
+        <div class="navbar-nav">
+          <router-link class="nav-item nav-link" to="/cars">Cars</router-link>
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import CarsApp from './pages/CarsApp.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: '/cars' },
+  { path: '/cars', component: CarsApp }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
